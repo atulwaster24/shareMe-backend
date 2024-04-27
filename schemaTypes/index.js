700ce5b5e368771398ddc0178f4b3eee2ce1979e
atulwaster24 <78548597+atulwaster24@users.noreply.github.com>
2024-04-27 21:11:25 +0530

    Initial Commit

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import commentSchema from "./schemas/commentSchema";
+import pinSchema from "./schemas/pinSchema";
+import postedBySchema from "./schemas/postedBySchema";
+import saveSchema from "./schemas/saveSchema";
+import userSchema from "./schemas/userSchema";
+
+export const schemaTypes = [userSchema, pinSchema, commentSchema,saveSchema,postedBySchema];
